a1d2f5de9980eaf2fd1dd538ae79eaaaeee58bad
Betterme <313303303@qq.com>
2024-08-08 15:47:21 +0800

    init

--- free_api/routers/images.py
+++ free_api/routers/images.py
@@ -33,23 +33,27 @@ async def generate(
         auth: Optional[HTTPAuthorizationCredentials] = Depends(get_bearer_token),
         base_url: Optional[str] = Query("https://api.siliconflow.cn/v1"),
 ):
-    logger.debug(request)
+    logger.debug(request.model_dump_json(indent=4))
 
+    image_response = None
     try:
         if request.model.startswith(("stable-diffusion-3",)):
             request.model = REDIRECT_MODEL.get(request.model, request.model)
             image_response = await api_images.api_create_image(request)
-            return image_response
+
+        elif request.model.startswith(("flux-pro-max",)):
+            request.is_nsfw = False  # 不审核
+            image_response = await flux.create_image(request)
 
         elif request.model.startswith(("flux-pro",)):
             image_response = await flux.create_image(request)
-            return image_response
 
-        elif request.model.startswith(("flux",)):
-            request.model = REDIRECT_MODEL.get(request.model, request.model)
+        elif request.model.startswith(("flux-dev",)):  # 逆向
+            request.model = "black-forest-labs/FLUX.1-schnell"
             image_response = await api_images.create_image(request)
-            return image_response
 
+        elif request.model.startswith(("kolors",)):
+            image_response = await kolors.create_image(request)
 
         elif request.model.startswith(("kling",)):
             kling_request = KlingaiImageRequest(
@@ -61,20 +65,13 @@ async def generate(
             )
             images = await klingai.create_image(kling_request)
             image_response = ImagesResponse(created=int(time.time()), data=images)
-            return image_response
-
-        elif request.model.startswith(("kling",)):
-            image_response = await kolors.create_image(request)
-            return image_response
-
-        else:
-            image_response = await api_images.api_create_image(request)  # 自动翻译成英文
-            return image_response
 
     except Exception as e:
-        send_message(f"SD兜底：{e}", title=__name__)
-        image_response = await api_images.api_create_image(request)  # 自动翻译成英文
-        return image_response
+        send_message(f"flux兜底：{e}", title=__name__)
+        # todo：触发内容监控
+        # https://oss.ffire.cc/files/jin.webp
+
+    return image_response or await api_images.api_create_image(request)  # 自动翻译成英文
 
 
 if __name__ == '__main__':
