27ed3814e333e4ceddd7cade1692e877ec6afd8b
Betterme <313303303@qq.com>
2024-08-23 18:01:07 +0800

    init

--- free_api/routers/images.py
+++ free_api/routers/images.py
@@ -15,7 +15,8 @@ from meutils.schemas.kuaishou_types import KlingaiImageRequest, KolorsRequest
 
 from meutils.apis.kuaishou import klingai
 from meutils.apis.hf import kolors
-from meutils.apis.siliconflow import api_images
+from meutils.apis.ideogram import ideogram_images
+from meutils.apis.siliconflow import api_images, image_to_image
 
 from meutils.notice.feishu import send_message
 
@@ -51,7 +52,8 @@ async def generate(
 
         elif request.model.startswith(("flux-pro",)):
             request.model = "black-forest-labs/FLUX.1-dev"
-            request.size = '1366x1366'
+            if request.size in {'1024x1024', '1:1'}:
+                request.size = "1366x1366"
 
             image_response = await api_images.create_image(request)
 
@@ -63,7 +65,10 @@ async def generate(
         elif request.model.startswith(("kolors",)):
             image_response = await kolors.create_image(request)
 
-        elif request.model.startswith(("kling",)):
+        elif request.model.startswith(("ideogram",)):
+            image_response = await ideogram_images.create(request)
+
+        elif request.model.startswith(("kling",)):  # 支持图生图
             kling_request = KlingaiImageRequest(
                 prompt=request.prompt,
                 imageCount=request.n,
@@ -76,6 +81,9 @@ async def generate(
             )
             images = await klingai.create_image(kling_request)
             image_response = ImagesResponse(created=int(time.time()), data=images)
+
+        elif request.url:  # 支持图生图
+            return await image_to_image.create(request)
 
     except Exception as e:
         send_message(f"flux兜底：{e}", title=__name__)
