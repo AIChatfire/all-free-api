765e28de4edfad080f86252fa0882e493c8c14cc
Betterme <313303303@qq.com>
2024-08-26 09:47:47 +0800

    init

--- free_api/routers/images.py
+++ free_api/routers/images.py
@@ -16,7 +16,7 @@ from meutils.schemas.kuaishou_types import KlingaiImageRequest, KolorsRequest
 from meutils.apis.kuaishou import klingai
 from meutils.apis.hf import kolors
 from meutils.apis.ideogram import ideogram_images
-from meutils.apis.siliconflow import api_images, image_to_image
+from meutils.apis.siliconflow import text_to_image, image_to_image
 
 from meutils.notice.feishu import send_message
 
@@ -36,36 +36,32 @@ async def generate(
 ):
     logger.debug(request.model_dump_json(indent=4))
 
-    image_response = None
     try:
         if request.model.startswith(("stable-diffusion", "dreamshaper")) and not request.url:
             request.model = REDIRECT_MODEL.get(request.model, request.model)
-            image_response = await api_images.api_create_image(request)
-
-        # elif request.model.startswith(("flux-pro-max",)):
-        #     request.nsfw_level = "6"  # 不审核
-        #     image_response = await mystic.create_image(request)
-        #
-        # elif request.model.startswith(("flux-pro",)):
-        #     # image_response = await flux.create_image(request)
-        #     image_response = await mystic.create_image(request)
+            image_response = await text_to_image.create(request)
+            return image_response
 
         elif request.model.startswith(("flux-pro",)):
             request.model = "black-forest-labs/FLUX.1-dev"
             if request.size in {'1024x1024', '1:1'}:
                 request.size = "1366x1366"
-            image_response = await api_images.create_image(request)
+            image_response = await text_to_image.create(request)
+            return image_response
 
         elif request.model.startswith(("flux-dev",)):
             request.model = "black-forest-labs/FLUX.1-dev"
-            image_response = await api_images.create_image(request)
+            image_response = await text_to_image.create(request)
+            return image_response
 
         elif request.model.startswith(("kolors",)):
             image_response = await kolors.create_image(request)
+            return image_response
 
         elif request.model.startswith(("ideogram",)):
             request.model = "V_1_5" if 'pro' in request.model else "V_0_3"
             image_response = await ideogram_images.create(request)
+            return image_response
 
         elif request.model.startswith(("kling",)):  # 支持图生图
             kling_request = KlingaiImageRequest(
@@ -80,16 +76,27 @@ async def generate(
             )
             images = await klingai.create_image(kling_request)
             image_response = ImagesResponse(created=int(time.time()), data=images)
+            return image_response
 
         elif request.url:  # 支持图生图
             return await image_to_image.create(request)
 
-    except Exception as e:
-        send_message(f"flux兜底：{e}", title=__name__)
-        # todo：触发内容监控
-        # https://oss.ffire.cc/files/jin.webp
+        else:
+            return await text_to_image.create(request)
 
-    return image_response or await api_images.api_create_image(request)  # 自动翻译成英文
+
+    except Exception as e:
+        send_message(f"images error：\n {e}", title=__name__)
+        if isinstance(e, httpx.HTTPStatusError):
+            return HTTPException(
+                status_code=e.response.status_code,
+                detail=e.response.text
+            )
+
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="未知错误，请联系管理员"
+        )
 
 
 if __name__ == '__main__':
