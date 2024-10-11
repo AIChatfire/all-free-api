803a2fd5e00629cc16c9c43e52b780bbf9e4a515
Betterme <313303303@qq.com>
2024-10-11 21:03:15 +0800

    init

--- free_api/routers/tools/watermark.py
+++ free_api/routers/tools/watermark.py
@@ -14,19 +14,14 @@
 
 
 from meutils.pipe import *
-from fastapi import FastAPI, HTTPException
-
-from urllib.parse import unquote
 
 from meutils.serving.fastapi.dependencies.auth import get_bearer_token, HTTPAuthorizationCredentials
 from meutils.llm.openai_utils import ppu_flow
 from meutils.apis import textin
-from meutils.io.files_utils import to_bytes, to_url
-from meutils.config_utils.lark_utils import get_next_token_for_polling
 from meutils.apis.baidu import bdaitpzs
+from meutils.io.files_utils import to_bytes, to_url
 
 from fastapi import APIRouter, Depends, BackgroundTasks, Query, Header
-from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 TAGS = ["图片编辑"]
@@ -39,14 +34,24 @@ async def remove_watermark(
 ):
     api_key = auth and auth.credentials or None
 
-    # 解码 URL
-    # url = unquote(url)
-
-    file = await to_bytes(url)
-
     async with ppu_flow(api_key, post="api-watermark-remove"):
-        data = await textin.document_process(file, service="watermark-remove")
-        return data
+        try:
+            request = bdaitpzs.BDAITPZSRequest(original_url=url, thumb_url=url)
+            data = await bdaitpzs.create_task(request, is_async=False, response_format="url")
+            url = data['url']
+            return {'code': 200,
+                    'data': {
+                        'file_data': '',
+                        'file_type': '',
+                        'result': {'image': url}
+                    },
+                    'msg': 'success'}
+        except Exception as e:
+            logger.error(e)
+
+            file = await to_bytes(url)
+            data = await textin.document_process(file, service="watermark-remove")
+            return data
 
 
 if __name__ == '__main__':
