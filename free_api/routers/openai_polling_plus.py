c2a3602338304f06895dfc5baa5edd7e86e0cd28
Betterme <313303303@qq.com>
2025-04-11 22:01:11 +0800

    init

--- free_api/routers/openai_polling_plus.py
+++ free_api/routers/openai_polling_plus.py
@@ -52,10 +52,10 @@ async def create_chat_completions(
 ):
     logger.debug(request.model_dump_json(exclude_none=True, indent=4))
 
-    with try_catch(f"{__name__}: {path}", api_key=api_key, headers=headers, request=request):
+    # https://all.chatfire.cc/g/openai
+    base_url = headers.get("base_url") or "https://api.siliconflow.cn/v1"
 
-        # https://all.chatfire.cc/g/openai
-        base_url = headers.get("base_url") or "https://api.siliconflow.cn/v1"
+    with try_catch(f"{base_url}/{path}", api_key=api_key, headers=headers, request=request):
 
         if path.endswith("images/generations"):
             pass
@@ -68,7 +68,7 @@ async def create_chat_completions(
                 model, redirect_model = request.model.split(":", maxsplit=1)
                 request.model = redirect_model
 
-            client = Completions(api_key=api_key, base_url=base_url)
+            client = Completions(base_url=base_url, api_key=api_key)
             response = await client.create(request)
 
             if request.stream:
@@ -91,6 +91,10 @@ if __name__ == '__main__':
 
 """
 
+{
+    "base_url": "https://all.chatfire.cc/g/openai"
+}
+
 
 curl -X 'POST' \
   'http://0.0.0.0:8000/v1/xx' \
@@ -107,6 +111,108 @@ curl -X 'POST' \
   ],
   "model": "xxxxxxxx:gemini-2.0-flash",
   "stream": true
+}'
+
+curl -X 'POST' \
+  'https://all.chatfire.cn/polling/v1/x' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer redis:https://xchatllm.feishu.cn/sheets/Bmjtst2f6hfMqFttbhLcdfRJnNf?sheet=kfKGzt' \
+  -H 'base_url: https://all.chatfire.cc/g/openai' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "gemini-2.0-flash",
+  "stream": true
+}'
+
+curl -X 'POST' \
+  'https://openai-dev.chatfire.cn/polling_plus/v1/x' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer redis:https://xchatllm.feishu.cn/sheets/Bmjtst2f6hfMqFttbhLcdfRJnNf?sheet=kfKGzt' \
+  -H 'base_url: https://all.chatfire.cc/g/openai' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "gemini-2.0-flash",
+  "stream": true
+}'
+
+
+curl -X 'POST' \
+  'https://openai-dev.chatfire.cn/polling_plus/v1/x' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer AIzaSyAcVutR1bYxUq8M9hqw4870t2W9zFthKkY' \
+  -H 'base_url: https://all.chatfire.cc/g/openai' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "gemini-2.0-flash",
+  "stream": true
+}'
+
+curl -X 'POST' \
+  'http://0.0.0.0:8000/v1/xx' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer redis:https://xchatllm.feishu.cn/sheets/Bmjtst2f6hfMqFttbhLcdfRJnNf?sheet=xlvlrH' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
+  "stream": true
+}'
+
+curl -X 'POST' \
+  'http://0.0.0.0:8000/v1/xx' \
+  -H 'accept: application/json' \
+  -H 'base_url: https://all.chatfire.cc/g/openai' \
+  -H 'Authorization: Bearer AIzaSyAcVutR1bYxUq8M9hqw4870t2W9zFthKkY' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "gemini-2.0-flash",
+  "stream": true
+}'
+
+curl -X 'POST' \
+  'http://0.0.0.0:8000/polling_plus/v1/xx' \
+  -H 'accept: application/json' \
+  -H 'base_url: https://all.chatfire.cc/g/openai' \
+  -H 'Authorization: Bearer AIzaSyAcVutR1bYxUq8M9hqw4870t2W9zFthKkY' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "讲个故事",
+      "role": "user"
+    }
+  ],
+  "model": "gemini-2.0-flash",
+  "stream": false
 }'
 
 """
