afe99f33b18b0ae85819364fb4f115b4eb54d45c
Betterme <313303303@qq.com>
2025-06-13 18:09:13 +0800

    init

--- free_api/routers/async_tasks/volcengine_apis.py
+++ free_api/routers/async_tasks/volcengine_apis.py
@@ -10,13 +10,19 @@
 
 
 from meutils.pipe import *
-from meutils.llm.openai_utils import ppu_flow
+from meutils.llm.openai_utils import ppu_flow, get_payment_times
+from meutils.llm.openai_utils.adapters import chat_for_video
+from meutils.llm.openai_utils import create_chat_completion, create_chat_completion_chunk, chat_completion
+
 from meutils.schemas.openai_types import CompletionRequest
+from meutils.decorators.contextmanagers import atry_catch
+from meutils.notice.feishu import send_message_for_dynamic_router as send_message
 
 from meutils.apis.volcengine_apis import tasks as volcengine_tasks
 from meutils.apis.volcengine_apis import videos
 
 from meutils.serving.fastapi.dependencies.auth import get_bearer_token
+from sse_starlette import EventSourceResponse
 from fastapi import APIRouter, File, UploadFile, Query, Form, Body, Depends, Request, HTTPException, status, \
     BackgroundTasks
 
@@ -32,7 +38,7 @@ TAGS = ["火山"]
 """
 
 
-@router.post("/{path:path}")
+@router.post("")
 async def generate(
         request: dict = Body(...),
         Action: str = Query(None, description="接口名称"),
@@ -50,22 +56,47 @@ async def generate(
             return task_reponse
 
 
-@router.post("/contents/generations/tasks/{task_id}")  # 通用类
-async def create_video_task(
+@router.get("/contents/generations/tasks/{task_id}")  # 通用类
+async def get_video_task(
         task_id: str,
 ):
     return await videos.get_task(task_id)
 
 
-@router.get("/contents/generations/tasks")  # 通用类
+@router.post("/contents/{dynamic_router:path}")  # 通用类
 async def create_video_task(
+        dynamic_router: str,  # /tasks
         request: CompletionRequest,
 
         api_key: Optional[str] = Depends(get_bearer_token),
 ):
-    async with ppu_flow(api_key, post=f"api-{request.model}", dynamic=True):
+    logger.debug(dynamic_router)
+    # logger.debug(bjson(request))
+    # chat模式
+    if dynamic_router.endswith("chat/completions"):
         response = await videos.create_task(request)
-        return response
+        task_id = response.get("id")  # {'id': 'cgt-20250611152553-r46ql'}
+
+        chunks = await chat_for_video(videos.get_task, task_id)
+        return EventSourceResponse(chunks)
+
+    N = get_payment_times(request)
+    # N = 0
+
+    dynamic_model = f"api-{request.model}"
+    if resolution := request.model_dump().get("resolution"):
+        dynamic_model += f"-{resolution}"
+    else:
+        for resolution in {"480p", "720p", "1080p"}:
+            if resolution in str(request):
+                dynamic_model += f"-{resolution}"
+                break
+
+    async with atry_catch(f"{dynamic_model}", api_key=api_key, callback=send_message, request=request):
+
+        async with ppu_flow(api_key, post=dynamic_model, dynamic=True, n=N):
+            response = await videos.create_task(request)
+            return response
 
 
 if __name__ == '__main__':
