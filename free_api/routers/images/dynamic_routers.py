d54e1e5ae305363dd6e10d0efd8fb606ad7e1de7
Betterme <313303303@qq.com>
2025-08-29 19:38:07 +0800

    init

--- free_api/routers/images/dynamic_routers.py
+++ free_api/routers/images/dynamic_routers.py
@@ -49,6 +49,7 @@ async def create_generations(
 
     base_url = base_url or headers.get("x-base-url")  # 环境变量
     # base_url = base_url or "https://api.huandutech.com/v1"
+    base_url="https://all.chatfire.cn/openrouter/v1"
 
     async with atry_catch(f"{dynamic_router}", base_url=base_url, api_key=api_key, callback=send_message,
                           request=request):
