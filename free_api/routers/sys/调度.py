07b77e210b1495c3412cc54a2b2b51a72cbdb42c
Betterme <313303303@qq.com>
2025-09-18 19:35:28 +0800

    init

---
+++ free_api/routers/sys/调度.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : 调度
+# @Time         : 2025/9/18 19:30
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 脚本
+
+from meutils.pipe import *
---
+++ scripts/refresh.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# @Project      : AI @by PyCharm
+# @Time         : 2025/9/18 19:34
+# @Author       : betterme
+# @Email        : 313303303@qq.com
+# @Software     : PyCharm
+# @Description  :
+
+docker run -d \
+  --name autoheal \
+  --restart always \
+  -e TZ=Asia/Shanghai \
+  -e AUTOHEAL_CONTAINER_LABEL=all \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  willfarrell/autoheal
+
+# 清空docker日志
+truncate -s 0 /var/lib/docker/containers/*/*-json.log
+
+# 显示当前所有镜像echo "当前所有镜像如下："docker images
+
+# 显示当前所有镜像
+echo "当前所有镜像如下："
+docker images
+
+# 删除无标签的镜像（即 <none>:<none> 的镜像）
+echo "正在删除无标签的镜像..."
+docker rmi $(docker images -f "dangling=true" -q)
+
+# 删除未被容器使用的镜像（谨慎操作，可能会删除一些你认为有用的镜像）
+echo "正在删除未被容器使用的镜像..."
+docker image prune -a -f
+
+echo "清理完成！"
