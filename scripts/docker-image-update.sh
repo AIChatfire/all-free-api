3a058d8645ce725f7b8ae22beede2d82df883eac
Betterme <313303303@qq.com>
2025-09-18 21:45:51 +0800

    init

---
+++ scripts/docker-image-update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# @Project      : AI @by PyCharm
+# @Time         : 2025/9/18 19:34
+# @Author       : betterme
+# @Email        : 313303303@qq.com
+# @Software     : PyCharm
+# @Description  :
+
+
---
+++ scripts/run-refresh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# @Project      : AI @by PyCharm
+# @Time         : 2025/9/18 20:11
+# @Author       : betterme
+# @Email        : 313303303@qq.com
+# @Software     : PyCharm
+# @Description  :
+
+curl -o refresh.sh https://raw.githubusercontent.com/AIChatfire/all-free-api/refs/heads/master/scripts/refresh.sh
+chmod +x refresh.sh && ./refresh.sh
