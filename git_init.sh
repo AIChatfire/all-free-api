248d48c5582a4f7306df9cc25887a379744d0468
Betterme <313303303@qq.com>
2025-10-22 22:03:31 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -6,32 +6,10 @@ git add .dockerignore .github/workflows/docker-image.yml
 
 git commit -m "init"
 
-
-
-
-
-
-
-
-
-
 #git rm  -r dist
 #git remote add origin git@github.com:yuanjie-ai/MeUtils.git
 #git branch -M master
 git push -u origin master -f
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
