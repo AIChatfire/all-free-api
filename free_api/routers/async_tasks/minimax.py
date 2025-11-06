f36058ce392023908b8d10f3fa718ae0e55d727a
Betterme <313303303@qq.com>
2025-11-06 14:23:17 +0800

    init

--- free_api/routers/async_tasks/minimax.py
+++ free_api/routers/async_tasks/minimax.py
@@ -64,11 +64,12 @@ async def create_task(
 
     # 计费
     billing_model = make_billing_model("minimax", request)
-    task_id = response.get("task_id")
-    await billing_for_async_task(billing_model, task_id=task_id, api_key=api_key)
+    if task_id := response.get("task_id"):
+        await billing_for_async_task(billing_model, task_id=task_id, api_key=api_key)
 
-    if len(str(request)) < 10000:  # 存储 request 方便定位问题
-        await redis_aclient.set(f"request:{task_id}", json.dumps(request), ex=7 * 24 * 3600)
+        if len(str(request)) < 10000:  # 存储 request 方便定位问题
+            await redis_aclient.set(f"request:{task_id}", json.dumps(request), ex=7 * 24 * 3600)
+
 
     return response
 
