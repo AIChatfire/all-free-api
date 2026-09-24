a70326c00c2a4f893b2c0997d016f9b12964b87f
Betterme <313303303@qq.com>
2026-09-24 16:19:01 +0800

    init

--- free_api/routers/dynamic/dynamic_async_tasks.py
+++ free_api/routers/dynamic/dynamic_async_tasks.py
@@ -79,7 +79,7 @@ async def get_task(
 
     assert task_id, "task_id is required"
 
-    upstream_base_url = "https://newapi-0to1.livetest.cn/volcengine/3d/api/v3"
+    upstream_base_url = "https://newapi-0to1.livetest.cn/aivideomaker/api/v3"
     upstream_api_key = "sk-1ZPWxVdbwBc8pf4o0xHMYVDXrHJ4y0JxATArWj0Z7K28kg3O"
 
     if not upstream_api_key:
@@ -203,7 +203,7 @@ async def create_task(
         # 执行任务
         try:
             ##########
-            upstream_base_url = "https://newapi-0to1.livetest.cn/volcengine/3d/api/v3"
+            upstream_base_url = "https://newapi-0to1.livetest.cn/aivideomaker/api/v3"
             upstream_api_key = "sk-1ZPWxVdbwBc8pf4o0xHMYVDXrHJ4y0JxATArWj0Z7K28kg3O"
 
             response = await make_request(
